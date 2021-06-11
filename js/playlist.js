6c14f142259bf39945f4d91d28a62bcf373d3194
franciscojofre <jjofrelopez@udesa.edu.ar>
2021-06-11 03:27:10 -0300

    funcionalidad de la playlist con la funcion

--- js/playlist.js
+++ js/playlist.js
@@ -4,18 +4,21 @@ let cancionesAPlaylist = JSON.parse(recuperarDatos)
 let playlist = document.querySelector('.cajas-index')
 
 for(let i = 0; i < cancionesAPlaylist.length; i++){
-    let url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${cancionesAPlaylist[i]}`
-    fetch(url)
-        .then(function(response){
-            return response.json()
-        })
-        .then(function(data){
-            //console.log(data);
-            let resultados = "";
-            playlist.innerHTML += `<li class="cajitas-index"><img src="${data.album.cover_xl}" alt=""><p><a href="./detail-track.html">${data.title}</a></p></li>`
-        })
-        .catch(function(error){
-            console.log(error);
-        })
+    buscarYMostrarPlaylist(cancionesAPlaylist[i])
 }
 
+function buscarYMostrarPlaylist(id){
+    let url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${id}`
+        fetch(url)
+            .then(function(response){
+                return response.json()
+            })
+            .then(function(data){
+                //console.log(data);
+                let resultados = "";
+                playlist.innerHTML += `<li class="cajitas-index"><img src="${data.album.cover_xl}" alt=""><p><a href="./detail-track.html">${data.title}</a></p></li>`
+            })
+            .catch(function(error){
+                console.log(error);
+            })
+}
