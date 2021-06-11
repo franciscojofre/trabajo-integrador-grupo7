616006ef4858f437b32381b1daed9eafbffaf6e2
franciscojofre <jjofrelopez@udesa.edu.ar>
2021-06-11 12:59:57 -0300

    arregle player, detalle visual de detail track y detalles de css

--- js/detail-track.js
+++ js/detail-track.js
@@ -19,7 +19,7 @@ fetch(url)
                                    <li class="datitos-canciones"><a href="./detail-album.html?id=${data.album.id}"><h3>Album: ${data.album.title}</h3></a></li>`
 
         let reproductor = document.querySelector('.player')
-        reproductor.innerHTML += `<iframe title="deezer-widget" src="https://www.deezer.com/us/track/${data.id}" width="100%" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media; clipboard-write"></iframe>`
+        reproductor.innerHTML += `<iframe title="deezer-widget" src="https://widget.deezer.com/widget/dark/track/${data.id}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media; clipboard-write"></iframe>`
     })
     .catch(function(error){
         console.log(error);
